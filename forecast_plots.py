144a273b2278d2412382d8c5f8187ad8a2a12a05
Anna Zapaishchykova <zapaishchykova@gmail.com>
2020-04-03 17:23:58 -0400

    Update forecast_plots.py
    nice plot

--- forecast_plots.py
+++ forecast_plots.py
@@ -5,65 +5,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from ggplot import *
-
-# in case ggplot complains see solution here https://github.com/yhat/ggpy/issues/662
-'''
-p <- ggplot(data_country) +
-    geom_bar(data = data_country, aes(x = time, y = deaths), 
-             fill = "coral4", stat='identity', alpha=0.5) + 
-    geom_line(data = data_country, aes(x = time, y = estimated_deaths), 
-              col = "deepskyblue4") + 
-    geom_line(data = data_country_forecast, 
-              aes(x = time, y = estimated_deaths_forecast), 
-              col = "black", alpha = 0.5) + 
-    geom_ribbon(data = data_country, aes(x = time, 
-                                         ymin = death_min, 
-                                         ymax = death_max),
-                fill="deepskyblue4", alpha=0.3) +
-    geom_ribbon(data = data_country_forecast, 
-                aes(x = time, 
-                    ymin = death_min_forecast, 
-                    ymax = death_max_forecast),
-                fill = "black", alpha=0.35) +
-    geom_vline(xintercept = data_deaths$time[length(data_deaths$time)], 
-               col = "black", linetype = "dashed", alpha = 0.5) + 
-    #scale_fill_manual(name = "", 
-    #                 labels = c("Confirmed deaths", "Predicted deaths"),
-    #                 values = c("coral4", "deepskyblue4")) + 
-    xlab("Date") +
-    ylab("Daily number of deaths\n") + 
-    scale_x_date(date_breaks = "weeks", labels = date_format("%e %b")) + 
-    scale_y_continuous(trans='log10', labels=comma) + 
-    coord_cartesian(ylim = c(1, 100000), expand = FALSE) + 
-    theme_pubr() + 
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-    guides(fill=guide_legend(ncol=1, reverse = TRUE)) + 
-    annotate(geom="text", x=data_country$time[length(data_country$time)]+8, 
-             y=10000, label="Forecast",
-             color="black")
-  print(p)
-'''
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_forecast(data_country):#, data_country_forecast):
-    g_bar = ggplot(data_country, aes(x='time', weight='deaths')) + geom_bar(colour='red', fill='red') + ggtitle("Deaths") #+ stat_smooth(colour='blue', span=0.2)
-    #g_line1 = ggplot(aes(x='time', y='estimated_deaths'), data=data_country) + geom_line() #+ stat_smooth(colour='blue', span=0.2)
-    #g_line2 = ggplot(aes(x='time', y='estimated_deaths_forecast'), data=data_country_forecast) + geom_line()
+def plot_forecasts(data_country):
+    y1_upper = np.asarray(df['deaths'] * 1.25)
+    y1_lower = np.asarray(df['deaths'] * 0.75)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-    print(g_bar)
-    #print(g_line1)
-    #print(g_line2)
+    ax.plot(data_country['time'],data_country['deaths'],'-g', alpha=0.6)  # solid green
+    ax.plot(data_country['time'],y1_lower,'-c', alpha=0.2)
+    ax.plot(data_country['time'],y1_upper,'-c', alpha=0.2)
+    ax.fill_between(data_country['time'], y1_lower, y1_upper, alpha=0.2)
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+
+    ax.bar(data_country['time'],data_country['deaths'],color='g',width=0.3,alpha=0.3)
+    ax.set_ylabel("Deaths")
+    ax.set_xlabel("Date")
+
+    plt.show()
 
 
-#here i make somy dummy data
-dates = [pd.to_datetime('2020-03-16'), pd.to_datetime('2020-03-17'), pd.to_datetime('2020-03-18'),
-         pd.to_datetime('2020-03-19'), pd.to_datetime('2020-03-20'), pd.to_datetime('2020-03-21'),
-         pd.to_datetime('2020-03-22'), pd.to_datetime('2020-03-23'), pd.to_datetime('2020-03-24')]
+# fill with dumb data
+dates = ['2020-03-16', '2020-03-17', '2020-03-18',
+         '2020-03-19', '2020-03-20', '2020-03-21',
+         '2020-03-22', '2020-03-23', '2020-03-24']
 
+deaths = [1, 5, 10, 20, 100, 200, 250, 380, 500]
 data_country = {'time':	dates,
-                'deaths': [1, 5, 10, 20, 100, 160, 170, 210, 300],
-                'estimated_deaths':[1, 5, 10, 100, 150, 160, 170, 190, 200]}
+                'deaths': deaths}
 
 df = pd.DataFrame(data=data_country)
-plot_forecast(df)#,data_country_forecast)
+plot_forecasts(df)
