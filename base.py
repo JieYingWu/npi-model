da3e384ae27f1b59402a5b7eeff2096ec96f480e
JieYing Wu <jieying@jhu.edu>
2020-04-03 10:56:40 -0400

    Updating instructions

--- base.py
+++ base.py
@@ -49,7 +49,7 @@ sm = pystan.StanModel(file='stan-models/base.stan')
 data_dir = sys.argv[1]
 
 countries = ['Denmark', 'Italy', 'Germany', 'Spain', 'United Kingdom', 'France', 'Norway', 'Belgium', 'Austria', 'Sweden', 'Switzerland']
-serial.interval = read.csv(join(data_dir, 'serial_interval.csv')) # Time between primary infector showing symptoms and secondary infected showing symptoms
+serial.interval = read.csv(join(data_dir, 'serial_interval.csv')) # Time between primary infector showing symptoms and secondary infected showing symptoms - this is a probability distribution from 1 to 100 days
 
 interventions = np.loadtxt(join(data_dir, 'interventions.csv'))
 ## TODO: They check that if any measure has not been in place until lockdown, set that intervention date to lockdown
@@ -87,13 +87,47 @@ stan_data = list(M=length(countries),N=NULL,p=p,x1=poly(1:N2,2)[,1],x2=poly(1:N2
                  y=NULL,covariate1=NULL,covariate2=NULL,covariate3=NULL,covariate4=NULL,covariate5=NULL,covariate6=NULL,covariate7=NULL,deaths=NULL,f=NULL,
                  N0=6,cases=NULL,LENGTHSCALE=7,SI=serial.interval$fit[1:N2],
                  EpidemicStart = NULL) # N0 = 6 to make it consistent with Rayleigh
-#stan_data = [len(countries), N=N, p=interventions.shape[1]-1,...]
+#stan_data = {'M':len(countries), 'N':N, 'p':interventions.shape[1]-1,...}
 
-# Train the model and generate samples
+# Train the model and generate samples - returns a StanFit4Model
 fit = sm.sampling(data=data, iter=200, chains=4, warmup=100, thin=4, seed=101, control={adapt_delta:0.9, max_treedepth:10})
 
 ## TODO: Read out the data of the stan model
+# Seems like extract parameters by str of the parameter name: https://pystan.readthedocs.io/en/latest/api.html#stanfit4model
+# Check that Rhat is close to 1 to see if the model's converged
 
+# summary_dict = fit.summary()
+# df = pd.DataFrame(summary_dict['summary'], 
+#                  columns=summary_dict['summary_colnames'], 
+#                  index=summary_dict['summary_rownames'])
 
+# alpha_mean, beta_mean = df['mean']['alpha'], df['mean']['beta']
+
+# Extracting traces
+# alpha = fit['alpha']
+# beta = fit['beta']
+# sigma = fit['sigma']
+# lp = fit['lp__']
+
+real<lower=0> mu[M]; // intercept for Rt
+real<lower=0> alpha[6]; // the hier term
+real<lower=0> kappa;
+real<lower=0> y[M];
+real<lower=0> phi;
+real<lower=0> tau;
+
+prediction = out$prediction
+estimated.deaths = out$E_deaths
+estimated.deaths.cf = out$E_deaths0
+
+JOBID = Sys.getenv("PBS_JOBID")
+if(JOBID == "")
+  JOBID = as.character(abs(round(rnorm(1) * 1000000)))
+print(sprintf("Jobid = %s",JOBID))
+
+save.image(paste0('results/',StanModel,'-',JOBID,'.Rdata'))
+
+save(fit,prediction,dates,reported_cases,deaths_by_country,countries,estimated.deaths,estimated.deaths.cf,out,covariates,file=paste0('results/',StanModel,'-',JOBID,'-stanfit.Rdata'))
 
 ## TODO: Make pretty plots
+# Probably don't have to use Imperial data for this, just find similar looking Python packages
