1ec423835589894b492f42bf0c10410cd8f0c677
JieYing Wu <jieying@jhu.edu>
2020-04-03 20:26:13 -0400

    Sampling...

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 from os.path import join, exists
 import sys
 import numpy as np
+from parser import get_stan_parameters
 
 import pystan
 import matplotlib.pyplot as plt
@@ -51,24 +52,25 @@ sm = pystan.StanModel(file='stan-models/base.stan')
 data_dir = sys.argv[1]
 
 countries = ['Denmark', 'Italy', 'Germany', 'Spain', 'United Kingdom', 'France', 'Norway', 'Belgium', 'Austria', 'Sweden', 'Switzerland']
-serial.interval = read.csv(join(data_dir, 'serial_interval.csv')) # Time between primary infector showing symptoms and secondary infected showing symptoms - this is a probability distribution from 1 to 100 days
+#serial.interval = np.loadtxt(join(data_dir, 'serial_interval.csv')) # Time between primary infector showing symptoms and secondary infected showing symptoms - this is a probability distribution from 1 to 100 days
 
-interventions = np.loadtxt(join(data_dir, 'interventions.csv'))
+#interventions = np.loadtxt(join(data_dir, 'interventions.csv'))
 ## TODO: They check that if any measure has not been in place until lockdown, set that intervention date to lockdown
 ## TODO: Build an array of intervention days where rows are the countries and columns are the interventions in this order: school/uni closures, self-isolating if ill, bannig public events, any government intervention, complete/partial lockdown, and social distancing/isolation
 
-weighted_fatalities = np.loadtxt(join(data_dir, 'weighted_fatality.csv'))
+weighted_fatalities = np.loadtxt(join(data_dir, 'weighted_fatality.csv'), skiprows=1, delimiter=',', dtype=str)
+ifrs = {}
+
+for i in range(weighted_fatalities.shape[0]):
+    ifrs[weighted_fatalities[i,1]] = float(weighted_fatalities[i,-2])
+
 ## TODO: I think we just need the weight fatailites column to calculate probability of death
 
-covid_up_to_date = np.loadtxt(join(data_dir, 'COVID-19-up-to-date.csv'))
-## TODO: covid_up_to_date is in a horrendous format - might consider reshaping for later use
-## TODO: Extract start date of relevant countries
+stan_data = get_stan_parameters()
 
-## TODO: Build infections/deaths timeseries for relevant countries
-# cases = 
-# deaths = 
+print(stan_data.keys())
 
-N = shape(cases)[1]
+#N = 
 N2 = 75
 
 ## TODO: turn rgammAlt, ecdf, and function thing into Python gamma distribution and convolution
@@ -80,33 +82,41 @@ mean2 = 18.8
 cv2 = 0.45 
 
 for c in countries:
-    ifr = weighted_fatalities[c]
+    ifr = ifrs[c]
     
     h = np.zeros(N2) # Discrete hazard rate from time t = 1, ..., 100
 
     ## assume that IFR is probability of dying given infection
-    x1 = np.random.gamma(mean1, cv1, 5e6) # infection-to-onset -> do all people who are infected get to onset?
-    x2 = np.random.gamma(mean2, cv2, 5e6) # onset-to-death
+    x1 = np.random.gamma(mean1, cv1, 5000000) # infection-to-onset -> do all people who are infected get to onset?
+    x2 = np.random.gamma(mean2, cv2, 5000000) # onset-to-death
 
     f = ECDF(x1+x2)
 
     def conv(u): # IFR is the country's probability of death
         return ifr * f(u)
     
-    h[1] = (conv(1.5) - conv(0))
+    h[0] = (conv(1.5) - conv(0))
     
-    for(i in 2:length(h)):
-        h[i] = (convolution(i+.5) - convolution(i-.5)) / (1-convolution(i-.5))
+    for i in range(1, N2):
+        h[i] = (conv(i+.5) - conv(i-.5)) / (1-conv(i-.5))
 
+    s = np.zeros(N2)
+    s[0] = 1
+    for i in range(1, N2):
+        s[i] = s[i-1]*(1-h[i-1])
+    f = s * h
+
+stan_data['f'] = f
+print(stan_data.keys())
 ## TODO: fill in the data for the stan model - check if Python wants something different
-stan_data = list(M=length(countries),N=NULL,p=p,x1=poly(1:N2,2)[,1],x2=poly(1:N2,2)[,2],
-                 y=NULL,covariate1=NULL,covariate2=NULL,covariate3=NULL,covariate4=NULL,covariate5=NULL,covariate6=NULL,covariate7=NULL,deaths=NULL,f=NULL,
-                 N0=6,cases=NULL,LENGTHSCALE=7,SI=serial.interval$fit[1:N2],
-                 EpidemicStart = NULL) # N0 = 6 to make it consistent with Rayleigh
+#stan_data = list(M=length(countries),N=NULL,p=p,x1=poly(1:N2,2)[,1],x2=poly(1:N2,2)[,2],
+#                 y=NULL,covariate1=NULL,covariate2=NULL,covariate3=NULL,covariate4=NULL,covariate5=NULL,covariate6=NULL,covariate7=NULL,deaths=NULL,f=NULL,
+#                 N0=6,cases=NULL,LENGTHSCALE=7,SI=serial.interval$fit[1:N2],
+#                 EpidemicStart = NULL) # N0 = 6 to make it consistent with Rayleigh
 #stan_data = {'M':len(countries), 'N':N, 'p':interventions.shape[1]-1,...}
 
 # Train the model and generate samples - returns a StanFit4Model
-fit = sm.sampling(data=stan_data, iter=200, chains=4, warmup=100, thin=4, seed=101, control={adapt_delta:0.9, max_treedepth:10})
+fit = sm.sampling(data=stan_data, iter=200, chains=4, warmup=100, thin=4, seed=101, control={'adapt_delta':0.9, 'max_treedepth':10})
 
 ## TODO: Read out the data of the stan model
 # Seems like extract parameters by str of the parameter name: https://pystan.readthedocs.io/en/latest/api.html#stanfit4model
