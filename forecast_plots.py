76d88b3d8db57b16d9311117086e1ba65a09b7cd
Anna Zapaishchykova <zapaishchykova@gmail.com>
2020-04-05 18:47:26 -0400

    created infected plot

--- forecast_plots.py
+++ forecast_plots.py
@@ -59,57 +59,62 @@ def plot_forecasts(data_country):
     plt.show()
 
 
-def plot_forecasts_without_dates(row):
+def plot_forecasts_wo_dates_quantiles(row2_5,row25,row50,row75,row97_5):
     '''
     :param data_country: pandas DF that contains column 'deaths' and 'time'
     '''
-    ticks = range(0, np.shape(row)[0])
-    y1_upper = np.asarray(row * 1.25)
-    y1_lower = np.asarray(row * 0.75)
+    ticks = range(0, np.shape(row50)[0])
+    y1_upper50 = np.asarray(row75)
+    y1_lower50 = np.asarray(row25)
+    y1_upper25 = np.asarray(row97_5)
+    y1_lower25 = np.asarray(row2_5)
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    ax.plot(ticks, row, '-g', alpha=0.6)  # solid green
-    ax.plot(ticks, y1_lower, '-c', alpha=0.2)
-    ax.plot(ticks, y1_upper, '-c', alpha=0.2)
-    ax.fill_between(ticks, y1_lower, y1_upper, alpha=0.2)
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+    ax.plot(ticks, row50, '-b', alpha=0.5)
 
-    ax.bar(ticks, row, color='g', width=0.8, alpha=0.3)
-    ax.set_ylabel("Deaths")
+    #ax.plot(ticks, y1_lower25, '-g', alpha=0.1)
+    #ax.plot(ticks, y1_upper25, '-g', alpha=0.1)
+    ax.fill_between(ticks, y1_lower25, y1_upper25, alpha=0.25,color='b')
+
+    #ax.plot(ticks, y1_lower50, '-g', alpha=0.1)
+    #ax.plot(ticks, y1_upper50, '-g', alpha=0.1)
+    ax.fill_between(ticks, y1_lower50, y1_upper50, alpha=0.2, color='b')
+
+
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+    ax.bar(ticks, row50, color='r', width=0.9, alpha=0.3)
+    ax.set_ylabel("Daily number of infections")
     ax.set_xlabel("Date")
 
     plt.show()
 
 
-def trial_run():
-    # fill with dumb data
-    dates = ['2020-03-16', '2020-03-17', '2020-03-18',
-         '2020-03-19', '2020-03-20', '2020-03-21',
-         '2020-03-22', '2020-03-23', '2020-03-24',
-         '2020-03-25', '2020-03-26', '2020-03-27']
+def plot_daily_infections_num(path, num_of_country, days_to_predict):
+    df = pd.read_csv(path, delimiter=';',index_col=0)
+    row_names = list(df.index.tolist())
+    prediction_list = []
+    list2_5, list25, list50, list75, list97_5 = [],[],[],[],[]
+    county_number = str(num_of_country)+']'
+    for name in row_names:
+        if "prediction[" in name:
+        #if "E_deaths[" in name:
+            if name.split(",")[1] == county_number:
+                print(name)
+                rowData = df.loc[name, :]
+                prediction_list.append(rowData['mean'])
+                list2_5.append(rowData['2.5%'])
+                list25.append(rowData['25%'])
+                list50.append(rowData['50%'])
+                list75.append(rowData['75%'])
+                list97_5.append(rowData['97.5%'])
 
-    deaths_predicted = [1, 5, 10, 20, 100, 200, 250, 380, 500, 510, 520, 550]
-    deaths_confirmed = [1, 5, 10, 20, 100, 190, 220]
-    data_country = {'time':	dates,
-                'deaths': deaths_predicted}
-
-    df = pd.DataFrame(data=data_country)
-    # example usage
-    plot_forecasts(df)
+                if name.split(",")[0] == ("prediction[" + str(days_to_predict)):
+                    break
+    plot_forecasts_wo_dates_quantiles(list2_5, list25, list50, list75, list97_5)
 
 
-df = pd.read_csv(r"D:\JHU\corona\npi-model\npi-model\summary_europe.csv", delimiter=';',index_col=0)
-#print(df.head())
-row_names = list(df.index.tolist())
-#print(row_names)
-prediction_list = []
-county_number = '1]'
-for name in row_names:
-    if "prediction[" in name:
-        if name.split(",")[1] == county_number:
-            print(name)
-            rowData = df.loc[name, :]
-            prediction_list.append(rowData['mean'])
-prediction_list = np.array(prediction_list)
-plot_forecasts_without_dates(prediction_list)
+path = r"D:\JHU\corona\npi-model\npi-model\summary_europe.csv"
+num_of_country = 1
+days_to_predict = 35
+plot_daily_infections_num(path,num_of_country,days_to_predict)
