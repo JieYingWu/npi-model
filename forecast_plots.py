b0f8a514817c24af8c0ece7614b9781f22d2c6e6
Anna Zapaishchykova <zapaishchykova@gmail.com>
2020-04-06 17:56:36 -0400

    added europe plots+general europe

--- forecast_plots.py
+++ forecast_plots.py
@@ -11,34 +11,11 @@ import numpy as np
 import datetime
 
 # this is for Europe
-'''
-dict_of_start_dates = {0: '02-22-2020', 1: '02-18-2020', 2: '02-21-2020', 3: '02-07-2020',
-                       4: '02-15-2020', 5: '01-27-2020', 6: '02-24-2020', 7: '02-09-2020',
-                       8: '02-18-2020', 9: '02-14-2020', 10: '02-12-2020'}
-                       
-dict_of_eu_geog = {0: 'Austria', 1: 'Belgium', 2: 'Denmark', 3: 'France',
-                   4: 'Germany', 5: 'Italy', 6: 'Norway', 7: 'Spain',
-                   8: 'Sweden', 9: 'Switzerland', 10: 'United_Kingdom'}
-                   
-start_day_of_confirmed ='12-31-2019'  # this will be different for US and Europe
-
-'''
+#start_day_of_confirmed ='12-31-2019'
 # this is for states
-'''
-dict_of_start_dates = {0: '2-16-2020', 1: '2-26-2020', 2: '2-20-2020', 3: '2-18-2020', 4: '02-21-2020',
-                       5: '2-21-2020', 6: '2-22-2020', 7: '2-17-2020', 8: '2-02-2020', 9: '02-18-2020',
-                       10: '2-16-2020', 11: '2-27-2020', 12: '2-20-2020', 13: '2-21-2020', 14: '02-28-2020',
-                       15: '2-22-2020', 16: '2-25-2020', 17: '2-25-2020', 18: '2-26-2020', 19: '2-27-2020'}
-
-dict_of_eu_geog = {0: 36061, 1: 36119, 2: 36059, 3: 36103, 4: 17031, 5: 26163, 6: 36087, 7: 34003,
-                   8: 53033, 9: 6037, 10: 22071, 11: 36071, 12: 9001, 13: 34013, 14: 12086,
-                   15: 26125, 16: 25017, 17: 34017, 18: 25025, 19: 34023}
-'''
-start_day_of_confirmed = '01-22-2020'  # this will be different for US and Europe
+start_day_of_confirmed = '01-22-2020'
 
 # input here variables for different plotting options
-#num_of_country = 3  # start from 1 !!!!!!!
-#plot_choice = 0  # 1 for deaths forecast; 0 for infections forecast
 base_model = True  # True for prediction/E_deaths, False for prediction0/E_deaths0
 last_day_to_plot = '03-31-2020'  # predict to this date
 
@@ -85,7 +62,7 @@ def plot_forecasts_wo_dates_quantiles(row2_5, row25, row50, row75, row97_5, conf
     ax.set_xlabel("Date")
 
     if county_name == "":
-        geography_name = str(dict_of_eu_geog[num_of_country - 1])
+        geography_name = str(dict_of_eu_geog[num_of_country])
     else:
         geography_name = str(county_name)
     ax.title.set_text(geography_name)
@@ -139,7 +116,7 @@ def plot_daily_infections_num(path, confirmed_cases, county_name, plot_choice, n
     plot_forecasts_wo_dates_quantiles(list2_5, list25, list50, list75, list97_5, confirmed_cases, county_name, plot_choice, num_of_country,dict_of_start_dates,dict_of_eu_geog)
 
 
-def read_true_cases_europe():
+def read_true_cases_europe(plot_choice, num_of_country, dict_of_start_dates, dict_of_eu_geog):
     '''
     1 for deaths forecast
     0 for infections forecast
@@ -202,13 +179,32 @@ def make_all_us_plots():
     return
 
 
+def make_all_eu_plots():
+    dict_of_start_dates = {0: '02-22-2020', 1: '02-18-2020', 2: '02-21-2020', 3: '02-07-2020',
+                           4: '02-15-2020', 5: '01-27-2020', 6: '02-24-2020', 7: '02-09-2020',
+                           8: '02-18-2020', 9: '02-14-2020', 10: '02-12-2020'}
+
+    dict_of_eu_geog = {0: 'Austria', 1: 'Belgium', 2: 'Denmark', 3: 'France',
+                       4: 'Germany', 5: 'Italy', 6: 'Norway', 7: 'Spain',
+                       8: 'Sweden', 9: 'Switzerland', 10: 'United_Kingdom'}
+
+    path = "./summary_europe.csv"
+    start_day_of_confirmed ='12-31-2019'  # this will be different for US and Europe
+
+    for plot_choice in range(0,2):
+        print(plot_choice)
+        for num_of_country in dict_of_eu_geog.keys():
+            print(num_of_country)
+            confirmed_cases = read_true_cases_europe(plot_choice, num_of_country, dict_of_start_dates, dict_of_eu_geog)
+            plot_daily_infections_num(path, confirmed_cases, "", plot_choice, num_of_country, dict_of_start_dates, dict_of_eu_geog)
+    return
+
+
 def main():
-    # This is for Europe
-    # path = r"D:\JHU\corona\npi-model\npi-model\summary_europe.csv"
-    # confirmed_cases = read_true_cases_europe()
-    # county_name = ""
-    # plot_daily_infections_num(path, confirmed_cases, county_name)
+    # This is for USA
     make_all_us_plots()
+    # This is for Europe
+    #make_all_eu_plots()
 
 
 if __name__ == '__main__':
