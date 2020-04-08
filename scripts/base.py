b1836d708b350f64d0b03f485f13bce12a9d1b74
Shreya <shreya.chak15@gmail.com>
2020-04-07 20:25:22 -0400

    Switch state and county

--- scripts/base.py
+++ scripts/base.py
@@ -22,17 +22,17 @@ if sys.argv[2] == 'europe':
     for i in range(weighted_fatalities.shape[0]):
         ifrs[weighted_fatalities[i,1]] = float(weighted_fatalities[i,-2])
 
-elif sys.argv[2] == 'US_state':
+elif sys.argv[2] == 'US_county':
     num_of_counties = 5
-    stan_data, countries = get_stan_parameters_by_state_us(num_of_counties, data_dir, show=False)
+    stan_data, countries = get_stan_parameters_by_county_us(num_of_counties, data_dir, show=False)
     weighted_fatalities = np.loadtxt(join(data_dir, 'us_data', 'weighted_fatality.csv'), skiprows=1, delimiter=',', dtype=str)
     ifrs = {}
     for i in range(weighted_fatalities.shape[0]):
         ifrs[str(weighted_fatalities[i,0])] = weighted_fatalities[i,-1]
 
-elif sys.argv[2] == 'US_county':
+elif sys.argv[2] == 'US_state':
     num_of_states = 20
-    stan_data, countries = get_stan_parameters_by_county_us(num_of_states, data_dir, show=False)
+    stan_data, countries = get_stan_parameters_by_state_us(num_of_states, data_dir, show=False)
     weighted_fatalities = np.loadtxt(join(data_dir, 'us_data', 'weighted_fatality.csv'), skiprows=1, delimiter=',', dtype=str)
     ifrs = {}
     for i in range(weighted_fatalities.shape[0]):
