3c0500cea32506306b1141ff08f95b005fd7fb98
JieYing Wu <jieying@jhu.edu>
2020-04-07 23:03:02 -0400

    Moved state weighted_fatality and fixed bug in dtype

--- scripts/base.py
+++ scripts/base.py
@@ -33,11 +33,13 @@ elif sys.argv[2] == 'US_county':
 elif sys.argv[2] == 'US_state':
     num_of_states = 20
     stan_data, countries = get_stan_parameters_by_state_us(num_of_states, data_dir, show=False)
-    weighted_fatalities = np.loadtxt(join(data_dir, 'state_data', 'weighted_fatality.csv'), skiprows=1, delimiter=',', dtype=str)
+    weighted_fatalities = np.loadtxt(join(data_dir, 'us_data', 'state_weighted_fatality.csv'), skiprows=1, delimiter=',', dtype=str)
     ifrs = {}
     for i in range(weighted_fatalities.shape[0]):
         ifrs[str(weighted_fatalities[i,0])] = weighted_fatalities[i,-1]
 
+stan_data['cases'] = np.array(stan_data['cases']).astype(np.int)
+stan_data['deaths'] = np.array(stan_data['deaths']).astype(np.int)
 #exit()
 
 N2 = stan_data['N2']
