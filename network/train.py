602da7fac63b3e16245cfd80cb4f5cf0869a2bfb
JieYing Wu <jieying@jhu.edu>
2020-04-27 09:56:06 -0400

    Caching fatality rates to avoid long computations

--- network/train.py
+++ network/train.py
@@ -1,4 +1,5 @@
 import sys
+import tqdm
 
 import torch
 import numpy as np
@@ -34,22 +35,22 @@ N2 = 100
 lr = 1e-4
 batch = 16
 use_previous_model = False
-train_counties = ['22071','36061','53033','34031','36059','06037','34003','17031','12086','34017','36103','34027','36119','48201','05119','22103','25017','12011','22033','34039','36087','09009','22095','42077','22017','44007','22105','09001','39101','22055']
-val_counties = ['22051','36029','22087','09003','26163']
+#train_counties = ['22071','36061','53033','34031','36059','06037','34003','17031','12086','34017','36103','34027','36119','48201','05119','22103','25017','12011','22033','34039','36087','09009','22095','42077','22017','44007','22105','09001','39101','22055']
+#val_counties = ['22051','36029','22087','09003','26163']
 # set up device 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-train_set = LSTMDataset(data_dir='data/us_data', counties=train_counties, split='train', retail_only=True, verbose=True)
-val_set = LSTMDataset(data_dir='data/us_data', counties=val_counties, split='val', retail_only=True, verbose=True)
+train_set = LSTMDataset(data_dir='data/us_data', split='train', retail_only=True, verbose=True)
+val_set = LSTMDataset(data_dir='data/us_data', split='val', retail_only=True, verbose=True)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch,
                                                     shuffle=False, num_workers=0)
         
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch,
                                                     shuffle=False, num_workers=0)
-train_regions = train_set.counties
-val_regions = val_set.counties
+train_regions = train_set.valid_fips_list
+val_regions = val_set.valid_fips_list
 regions = train_regions + val_regions
 
 # Read existing weights for both G and D models
@@ -75,27 +76,28 @@ wf_file = join(data_dir, 'us_data', 'weighted_fatality.csv')
 weighted_fatalities = pd.read_csv(wf_file, encoding='latin1', index_col='FIPS')
 #ifrs = weighted_fatalities[weighted_fatalities['FIPS'].isin(regions)]
 ifrs = {}
-print(weighted_fatalities.index)
 for r in regions:
     ifrs[r] = weighted_fatalities.loc[int(r), 'fatality_rate']
-print(ifrs)
 
 serial_interval = np.loadtxt(join(data_dir, 'serial_interval.csv'), skiprows=1, delimiter=',')
 
 lr = 1e-4
 batch = 16
 n_epochs = 500
+validate_each = 5
 
 model = NpiLstm(N2, FEATURES, HIDDEN_DIM, batch, OUTPUT_DIM)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+scheduler = ReduceLROnPlateau(optimizer)
 print(f'The model has {count_parameters(model):,} trainable parameters')
 
 loss_fn = NpiLoss(N2, regions, ifrs, serial_interval, device)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-scheduler = ReduceLROnPlateau(optimizer)
-
-all_loss = np.zeros(num_epochs)
+train_loss = np.zeros(num_epochs)
+val_loss = np.zeros(int(num_epochs/validate_each))
 
 for e in range(n_epochs):
+    tq = tqdm.tqdm(total=(len(train_loader) * batch))
+    tq.set_description('Epoch {}, lr {}'.format(e, lr))
     model.train()
     epoch_loss = 0
     for i, data in enumerate(train_loader):
@@ -103,26 +105,38 @@ for e in range(n_epochs):
         # Forward pass
         rt_pred = model(data['deaths'])
         loss = loss_fn(rt_pred, data['deaths'])
-        all_loss[t] = loss.item()
-
+        mean_loss = loss.item()
+        
         # Backward pass
         optimiser.zero_grad()
         loss.backward()
         optimiser.step()
 
-        # Validate
-        if e % validate_each == 0:
-            model.eval()
-            all_val_loss = []
-            with torch.no_grad():
-                for j, data in enumerate(val_loader):
+        tq.update(batch_size)
+        tq.set_postfix(loss=' loss={:.5f}'.format(mean_loss))
+        epoch_loss += mean_loss
 
-                    # Forward pass
-                    rt_pred = model(data['deaths'])
-                    loss = loss_fn(rt_pred, data['deaths'])
-                    all_val_loss.append(loss.item())
+    tq.set_postfix(loss=' loss={:.5f}'.format(epoch_loss/len(train_loader)))
+    train_loss[e] = epoch_loss
+    
+    # Validate
+    if e % validate_each == 0:
+        model.eval()
+        epoch_loss = 0
+        with torch.no_grad():
+            for j, data in enumerate(val_loader):
 
-            mean_loss = np.mean(all_val_loss)
-            scheduler.step(mean_loss)
-            model_path = model_root / "model_{}.pt".format(e)
-            save(e, model, model_path, mean_loss, optimizer, scheduler)
+                # Forward pass
+                rt_pred = model(data['deaths'])
+                loss = loss_fn(rt_pred, data['deaths'])
+                epoch_loss += loss.item()
+
+        mean_loss = epoch_loss / len(val_loader)
+        val_loss[e/validate_each] = mean_loss
+        tq.set_postfix(loss='validation loss={:5f}'.format(mean_loss))
+
+        scheduler.step(mean_loss)
+        model_path = model_root / "model_{}.pt".format(e)
+        save(e, model, model_path, mean_loss, optimizer, scheduler)
+
+    tq.close()
