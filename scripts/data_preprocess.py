cf6e38b6da77f892a225c2bac8f9bcd8608e7015
JieYing Wu <jieying@jhu.edu>
2020-05-01 14:46:16 -0400

    Refactored data_parser and data_preprocess and unused functions

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -5,7 +5,7 @@ from os.path import join, exists
 
 pd.set_option('mode.chained_assignment', None)
 
-def filter_negative_counts(df_cases, df_deaths, idx):
+def remove_negative_regions(df_cases, df_deaths, idx):
     """"
     Returns:
         df_cases: Infections time series with no negative values
@@ -27,7 +27,7 @@ def filter_negative_counts(df_cases, df_deaths, idx):
 
     return df_cases, df_deaths
 
-def filtering(df_cases, df_deaths, interventions, num_counties):
+def select_top_regions(df_cases, df_deaths, interventions, num_counties):
     """"
     Returns:
         df_cases: Infections timeseries for top N places
@@ -60,74 +60,6 @@ def filtering(df_cases, df_deaths, interventions, num_counties):
     #print("Inside filtering function:", df_cases.shape, df_deaths.shape)
     return df_cases, df_deaths, interventions, fips_list
 
-def check_monotonicity(L):
-    is_monotonic = np.sum([x<= y for x, y in zip(L, L[1:])])
-    return is_monotonic
-
-def county_monotonicity(df):
-    df1 = df.iloc[:, 2:].apply(check_monotonicity, axis=1)
-    df1 = df1[df1!=(df.shape[1] - 3)]
-    idx_row = {}
-    return df.iloc[df1.index]['FIPS']
-
-
-def simple_impute_data(arr):
-    """
-    Naive data imputation that does NOT yield a monotonically increasing timeseries
-
-    """
-
-    arr = arr.T
-    for county in arr:
-        for i, cell in enumerate(county[1:], 1):
-            if cell != -1 and county[i + 1] != -1 and cell < county[i - 1]:
-                county[i] = interpolate(county[i - 1], county[i + 1])
-    arr = arr.T
-    return arr
-
-
-def advanced_impute_data(arr):
-    """
-    Make array monotonically increasing by linear interpolation
-    Returns:
-    - Imputed Array
-
-    """
-
-    arr = arr.T
-    for county in arr:
-        change_list = []
-        # get first date of cases/deaths and skip it
-        first = np.nonzero(county)[0]
-        for i, cell in enumerate(county[1:], 1):
-            if i < first[0]:
-                continue
-            # Special Case where series is decreasing towards the end
-            if cell == -1 and len(change_list) != 1:
-                first_idx = change_list[0]
-                diff = county[first_idx] - county[first_idx - 1]
-                new_value = county[first_idx] + diff
-
-                for j, idx in enumerate(change_list[1:], 1):
-                    new_value += diff
-                    county[idx] = new_value
-                break
-
-            if cell != -1 and county[i + 1] != -1 and change_list == []:
-                change_list.append(i)
-
-            if i not in change_list:
-                change_list.append(i)
-            if cell > county[change_list[0]]:
-                if len(change_list) >= 3:
-                    # cut first and last value of change list
-                    first_, *change_list, last_ = change_list
-                    county[change_list[0]:change_list[-1] + 1] = interpolate(change_list,
-                                                                             county[first_],
-                                                                             county[last_])
-                    change_list = [last_]
-    arr = arr.T
-    return arr
 
 def impute(df, allow_decrease_towards_end=True):
     """
@@ -157,14 +89,14 @@ def impute(df, allow_decrease_towards_end=True):
     header = df.columns.values
     df = df.to_numpy()
     
-    for county in df:
+    for region in df:
         change_list = []
         # get first date of cases/deaths and skip it
-        first = np.nonzero(county)[0]
+        first = np.nonzero(region)[0]
         if len(first) == 0:
             continue
         change_list.append(first[0])
-        for i, cell in enumerate(county[1:], 1):
+        for i, cell in enumerate(region[1:], 1):
             if i < first[0]:
                 continue
 
@@ -173,26 +105,26 @@ def impute(df, allow_decrease_towards_end=True):
 
 
             if cell < 0:
-                county[i] = 0
+                region[i] = 0
 
             # Special Case where series is decreasing towards the end
-            if i == (len(county)-1) and len(change_list) > 1 and not allow_decrease_towards_end:
+            if i == (len(region)-1) and len(change_list) > 1 and not allow_decrease_towards_end:
                 first_idx = change_list[0]
-                diff = county[first_idx] - county[first_idx - 1]
-                new_value = county[first_idx] + diff
+                diff = region[first_idx] - region[first_idx - 1]
+                new_value = region[first_idx] + diff
 
                 for j, idx in enumerate(change_list[1:], 1):
-                    county[idx] = new_value
+                    region[idx] = new_value
                     new_value += diff
                 break
 
-            if cell > county[change_list[0]]:
+            if cell > region[change_list[0]]:
                 if len(change_list) >= 3:
                     # cut first and last value of change list
                     first_, *change_list, last_ = change_list
-                    county[change_list[0]:change_list[-1] + 1] = interpolate(change_list,
-                                                                             county[first_],
-                                                                             county[last_]) 
+                    region[change_list[0]:change_list[-1] + 1] = interpolate(change_list,
+                                                                             region[first_],
+                                                                             region[last_]) 
                     change_list = [last_]
                 else:
                     change_list = [change_list[-1]]
@@ -225,12 +157,8 @@ def preprocessing_us_data(data_dir, mode='county'):
         interventions: Interventions data with dates converted to date format
     """
     assert mode in ['county', 'state'], ValueError()
-    if mode == 'county':
-        cases_path = join(data_dir, 'us_data/infections_timeseries.csv')
-        deaths_path = join(data_dir, 'us_data/deaths_timeseries.csv')
-    else:
-        cases_path = join(data_dir, 'us_data/infections_timeseries_w_states.csv')
-        deaths_path = join(data_dir, 'us_data/deaths_timeseries_w_states.csv')
+    cases_path = join(data_dir, 'us_data/infections_timeseries_w_states.csv')
+    deaths_path = join(data_dir, 'us_data/deaths_timeseries_w_states.csv')
         
         
     interventions_path = join(data_dir, 'us_data/interventions.csv')
@@ -248,8 +176,6 @@ def preprocessing_us_data(data_dir, mode='county'):
     for col in int_cols: ### convert date from given format
         interventions[col] = interventions[col].apply(lambda x: dt.date.fromordinal(int(x)))
 
-    #issues_cases = county_monotonicity(df_cases)
-    #issues_deaths = county_monotonicity(df_deaths)
     def get_daily_counts(L):
         diff = np.array([y - x for x, y in zip(L, L[1:])])
         L[1:] = diff
