d748120b68c266a4d75de76ede3ec02c606f8e4a
JieYing Wu <jieying@jhu.edu>
2020-05-06 19:09:20 -0400

    Adding timeseries calculation

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 def get_cluster(filename, cluster_num):
@@ -5,6 +6,27 @@ def get_cluster(filename, cluster_num):
     fips = df.loc[df['cluster'] == cluster_num, 'FIPS']
     return fips.values
 
+def calculate_rt(r0, alphas, interventions):
+    rt = r0 - alpahs * covariates
+    return rt
+
+def calculate_cases(rt, population, si)
+    n = len(rt)
+    cases = np.zeros(n) 
+    for i in range(2,n):
+        case = np.sum(infected[0:i-1] * si[0:i-1])
+        rt_adj = (population -np.sum(infected)) / population * rt[i]
+        cases[i] = rt_adj * case
+    return cases
+
+def calculate_deaths(cases, ifr, fatality):
+    n = len(rt)
+    deaths = np.zeros(n)
+    deaths[0] = 1e-15*infected[0]
+    for i in range(1,n):
+        death = np.sum(cases[0:i-1] * fatality[0:i-1])
+        deaths[i] = ifr * death
+    return deaths    
 
 if __name__ == '__main__':
     filename = 'data/us_data/clustering.csv'
