750dfdba7605b57c7f0995f9aaa9afb4e9fd873d
JieYing Wu <jieying@jhu.edu>
2020-05-12 08:36:40 -0400

    Adding functions to get counties with isolated interventions

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from os.path import join
 
 def get_cluster(filename, cluster_num):
     df = pd.read_csv(filename)
@@ -28,8 +29,33 @@ def calculate_deaths(cases, ifr, fatality):
         deaths[i] = ifr * death
     return deaths    
 
+def get_npis(data_dir):
+    interventions_path = join(data_dir, 'us_data/interventions.csv')
+    interventions = pd.read_csv(interventions_path)
+    id_cols = ['FIPS', 'STATE', 'AREA_NAME']    
+    int_cols = [col for col in interventions.columns.tolist() if col not in id_cols]
+    interventions.drop(columns=['STATE', 'AREA_NAME'], inplace=True)
+
+    interventions.drop([0], axis=0, inplace=True)
+    interventions.fillna(1, inplace=True)
+    return interventions
+
+def get_counties_isolated_NPIs(npis, index):
+    cur_npi_dates = np.array(npis[index].values)
+    dates_diff =  abs(np.array(npis.iloc[:,1:].values).transpose() - cur_npi_dates).transpose()
+    dates_diff.sort(axis=1)
+    to_keep = npis[dates_diff[:,1] > 2]
+    return to_keep['FIPS']
+
+    
 if __name__ == '__main__':
-    filename = 'data/us_data/clustering.csv'
-    cluster_num = 5
-    fips = get_cluster(filename, cluster_num)
-    print(fips)
+#    filename = 'data/us_data/clustering.csv'
+#    cluster_num = 5
+#    fips = get_cluster(filename, cluster_num)
+#    print(fips)
+
+    
+    data_dir = 'data'
+    interventions = get_npis(data_dir)
+    counties = get_counties_isolated_NPIs(interventions, 'stay at home')
+    print(counties)
