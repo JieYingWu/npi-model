c675422fe5fac29398ed2c8caec6e8933592ced6
Anna Zapaishchykova <zapaishchykova@gmail.com>
2020-05-20 04:21:08 -0400

    Update plot_rt_over_density.py
    added maching to Rnow

--- scripts/plot_rt_over_density.py
+++ scripts/plot_rt_over_density.py
@@ -66,39 +66,45 @@ def read_density(path_density, selected_dict, dict_r0_supercounty):
     dict_r0 = {}
     for key in selected_dict.keys():
         for county in selected_dict[key]:
-            density_dict[county] = df.at[int(county), 'transit_scores - population weighted averages aggregated from town/city level to county']
+            density_dict[county] = df.at[int(county), 'Density per square mile of land area - Housing units']
             dict_r0[county] = dict_r0_supercounty[key]
     return density_dict, dict_r0
 
 
-def get_rt_adj(path, geo_list):
+def get_rt_adj(path, geo_list, start_day_dict):
     # 1 for deaths; 0 for infections
     plot_name = "Rt_adj["
     df = pd.read_csv(path+"\\summary.csv", delimiter=',', index_col=0)
     row_names = list(df.index.tolist())
     means = {}
-    start_day = 1
+    #start_day = 1
     for num_of_country in range(0, len(geo_list)):
         county_number = str(int(num_of_country) + 1) + "]"
         for name in row_names:
             if plot_name in name and name.split(",")[1] == county_number:
                 rowData = df.loc[name, :]
                 key = geo_list[num_of_country]
-                if name.split(",")[0] == (plot_name + str(start_day)):
+                if name.split(",")[0] == (plot_name + str(start_day_dict[key])):
                     means[key] = rowData['mean']
     return means
 
 
 def get_start_day_dict(path, geo_list):
     dict_start_days = {}
+    df = pd.read_csv(path + "\\start_dates.csv", delimiter=',', index_col=0)
+    supercounties_dates = list(df.loc[0][:])
+    set_day_for_search = datetime.datetime.strptime('5/18/20', '%m/%d/%y')
+    for i in range(0, len(geo_list)):
+        days_to_predict = (set_day_for_search - datetime.datetime.strptime(supercounties_dates[i], '%m/%d/%y')).days
+        dict_start_days[geo_list[i]] = days_to_predict
     return dict_start_days
 
 
-def plot_scatter(path):
+def plot_scatter_r0(path):
     path_density = r"D:\JHU\corona\npi-model\npi-model\data\us_data"
     fig = plt.figure('Density')
     ax = fig.add_subplot(111)
-    ax.set_xlabel("transit_scores - population weighted averages aggregated from town/city level to county")
+    ax.set_xlabel("Density per square mile of land area - Housing units")
     ax.set_ylabel("R0")
     colors = ["#377EB8", "#E41A1C", "#984EA3", "#4DAF4A", "#FF7F00"]
 
@@ -108,9 +114,6 @@ def plot_scatter(path):
         supercounties_dist, supercounties_names = create_geocodes_dict(path_rt, path_density)
         r0_means_list = get_means_list(path_rt, supercounties_names)
         print(r0_means_list)
-        #rt_adj_list = get_rt_adj(path_rt, supercounties_names)
-        #print(rt_adj_list)
-        #sum_density_dict = read_density(path_density, supercounties_dist)
         density_dict,dict_r0 = read_density(path_density, supercounties_dist, r0_means_list)
         print(density_dict)
         print(dict_r0)
@@ -122,21 +125,38 @@ def plot_scatter(path):
     plt.show()
 
 
+def plot_scatter_radj(path):
+    path_density = r"D:\JHU\corona\npi-model\npi-model\data\us_data"
+    fig = plt.figure('Density')
+    ax = fig.add_subplot(111)
+    ax.set_xlabel("Density per square mile of land area - Housing units")
+    ax.set_ylabel("R_now")
+    colors = ["#377EB8", "#E41A1C", "#984EA3", "#4DAF4A", "#FF7F00"]
+
+    for cluster_n in range(0, 5):
+        print(cluster_n)
+        path_rt = path + str(cluster_n)
+        supercounties_dist, supercounties_names = create_geocodes_dict(path_rt, path_density)
+
+        start_day_dict = get_start_day_dict(path_rt, supercounties_names)
+        rt_adj_list = get_rt_adj(path_rt, supercounties_names, start_day_dict)
+
+        density_dict, dict_r0 = read_density(path_density, supercounties_dist, rt_adj_list)
+        print(density_dict)
+        print(dict_r0)
+
+        for key in density_dict.keys():
+            x = density_dict[key]
+            y = dict_r0[key]
+            if x < 10000:
+                ax.scatter(x, y, color=colors[cluster_n], s=8, alpha=0.6)
+    plt.show()
+
 def main():
 
     path = r"D:\JHU\corona\npi-model\npi-model\results\table_no_validation\cluster"
-    path_density = r"D:\JHU\corona\npi-model\npi-model\data\us_data"
-    '''
-    path_rt = path + str(0)
-    supercounties_dist, supercounties_names = create_geocodes_dict(path_rt, path_density)
-    r0_means_list = get_means_list(path_rt, supercounties_names)
-    print(r0_means_list)
-    rt_adj_list = get_rt_adj(path_rt, supercounties_names)
-    start_day_dict = get_start_day_dict(path_rt, supercounties_names)
-    print(start_day_dict)
-    '''
-    plot_scatter(path)
-
+    plot_scatter_radj(path)
+    plot_scatter_r0(path)
 
 if __name__ == '__main__':
     # run from base directory
