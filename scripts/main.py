f0882de168c409a21efbb0884607a8993615c115
Benjamin Killeen <benjamindkilleen@gmail.com>
2020-07-05 14:44:20 -0400

    test brun

--- scripts/main.py
+++ scripts/main.py
@@ -360,7 +360,7 @@ class MainStanModel():
         stan_data['f'] = all_f
 
         fit = sm.sampling(data=stan_data, iter=self.iter, chains=5, warmup=self.warmup_iter,
-                          thin=1, control={'adapt_delta': 0.99, 'max_treedepth': self.max_treedepth})
+                          thin=4, control={'adapt_delta': 0.99, 'max_treedepth': self.max_treedepth})
         # fit = sm.sampling(data=stan_data, iter=2000, chains=4, warmup=10, thin=4, seed=101, control={'adapt_delta':0.9, 'max_treedepth':10})
 
         if validation:
