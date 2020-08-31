0004aaeb94986c40e7cd621af9a40d6792145716
JieYing Wu <jieying@jhu.edu>
2020-08-31 11:13:51 -0400

    Updated rhat plots and printing ci

--- scripts/generate_readable_results.py
+++ scripts/generate_readable_results.py
@@ -100,7 +100,7 @@ def make_readable_summary(result_dir, end_date, data_dir='data/us_data'):
 
   # first, report and save the alpha values:
   alpha_indices = list(filter(lambda x : re.match(r'alpha\[\d\]', x) is not None, indices))
-  alphas = summary.loc[alpha_indices, ['mean', 'sd']]
+  alphas = summary.loc[alpha_indices, ['mean', '2.5%', '97.5%']]
   alphas['NPI'] = ['$I_1$: Stay at home',
                    '$I_2$: >50 gathering',
                    '$I_3$: >500 gathering',
@@ -119,9 +119,9 @@ def make_readable_summary(result_dir, end_date, data_dir='data/us_data'):
 
   print(alphas)
   print('================================================================================')
-  print(f'alpha value [mean (std)] for copying:')
+  print(f'alpha value [mean (ci)] for copying:')
   print('================================================================================')
-  print('\n'.join('{:.03f} ({:.03f})'.format(row['mean'], row['sd']) for i, row in alphas.iterrows()))
+  print('\n'.join('{:.03f} ({:.03f}, {:,.03f}))'.format(row['mean'], row['2.5%'], row['97.5%']) for i, row in alphas.iterrows()))
   print('================================================================================')
   alphas.to_csv(join(result_dir, 'alphas.csv'))
 
